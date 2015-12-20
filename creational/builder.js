8ad596e1480cae5c75d365729228599f0067834d
Vitaly <v-a-komarov@yandex.ru>
2015-12-21 01:29:09 +0400

    Builder pattern implementation

--- creational/builder.js
+++ creational/builder.js
@@ -1,3 +1,38 @@
-/**
- * Created by Vega on 20.12.2015.
- */
+function BorderBuilder($el) {
+    this._$el = $el;
+    this._style = '';
+}
+
+BorderBuilder.prototype.width = function(width) {
+    this._style += ' ' + width;
+    return this;
+};
+
+BorderBuilder.prototype.color = function(color) {
+    this._style += ' ' + color;
+    return this;
+};
+
+BorderBuilder.prototype.lineStyle = function(lineStyle) {
+    this._style += ' ' + lineStyle;
+    return this;
+};
+
+BorderBuilder.prototype.set = function(lineStyle) {
+    this._$el.css('border', this._style);
+    this._style += ' ' + lineStyle;
+    return this;
+};
+
+function director(builder) {
+    builder
+        .width('3px')
+        .color('yellow')
+        .lineStyle('solid')
+        .set();
+}
+
+var $element = $('#abstract-factory');
+
+var borderBuilder = new BorderBuilder($element);
+
