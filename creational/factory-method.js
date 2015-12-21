2278fb9932caf23765945a7449b088b5d20a9d48
Vitaliy Komarov <vitaliy_komarov@epam.com>
2015-12-21 16:13:02 +0400

    Factory method pattern implementation

--- creational/factory-method.js
+++ creational/factory-method.js
@@ -1,3 +1,65 @@
-/**
- * Created by Vega on 20.12.2015.
- */
+/*
+* Abstract product
+*
+* Живое существо
+* */
+function Creature() {
+    // Abstract
+}
+
+Creature.prototype.speak = function() {
+    var speach = this._speach;
+    console.log(speach);
+};
+
+/*
+* Concrete products
+* */
+
+function Man() {
+    this._speach = 'Hello!';
+}
+
+function Dog() {
+    this._speach = 'Woof woof!';
+}
+
+function Snake() {
+    this._speach = 'Shhhhhh!'
+}
+
+Man.prototype = new Creature();
+Dog.prototype = new Creature();
+Snake.prototype = new Creature();
+
+/* Factory
+* */
+function God() {
+}
+/* Factory method
+* */
+
+God.prototype.create = function(type) {
+
+    var ProductClass;
+
+    switch (type) {
+        case 'man':
+            ProductClass = Man;
+            break;
+        case 'dog':
+            ProductClass = Dog;
+            break;
+        case 'snake':
+            ProductClass = Snake;
+            break;
+    }
+
+    return new ProductClass;
+};
+
+var god = new God();
+
+god.create('man').speak();
+god.create('dog').speak();
+god.create('snake').speak();
